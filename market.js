1be8ff035f373eee236c08983a431b4fe40b915b
liska228 <158029803+liska228@users.noreply.github.com>
2024-05-06 11:08:29 +0300

    o

---
+++ market.js
@@ -0,0 +1,37 @@
+
+// Перевірка підключеного файлу скриптів
+console.log('Перевірка підключеного файлу скриптів market.js')
+
+// Отримання елементу з ідентифікатором items
+let itemsDiv = document.getElementById("items");
+
+//Перевірка існування знайденого блоку 
+if(itemsDiv) {
+
+    //Вивід знайденого елементу 
+    console.log(itemsDiv)
+    // Вивід значення поля знайденого елементу
+    // console.log('Поле classList: ', itemsDiv.classList)
+    // console.log('Поле id: ', itemsDiv.id)
+    // console.log('Поле clientWidth: ', itemsDiv.clientWidth)
+    // console.log('Поле innerHTML: ', itemsDiv.innerHTML)
+
+    // // Додавання тексту в блок
+    // itemsDiv.innerText='Перший Програмно додано текст'
+    // itemsDiv.innerText+='Другий Програмно додано текст'
+
+    // //Додавання відформатованого HTML коду в блок 
+    // itemsDiv.innerHTML = '<h1>Відформатований HTML код</h1>'
+    // itemsDiv.innerHTML = '<div class = "item"></div>'
+    // itemsDiv.innerHTML += '<div class = "item"></div>' 
+    // itemsDiv.innerHTML += '<div class = "item"></div>' 
+    // itemsDiv.innerHTML += '<div class = "item"></div>'
+
+    for (let i =0; i<=100; i++){
+        itemsDiv.innerHTML += '<div class = "item"></div>'}  
+
+} else {
+
+    //Вивід повідомлення про не знайдений блок 
+    console.log("Блок товарів не знайдено")
+}
