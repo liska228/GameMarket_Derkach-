a9fc57d573baed455f086df906c283ca17af996e
liska228 <158029803+liska228@users.noreply.github.com>
2024-05-12 23:27:04 +0300

    k

--- market.js
+++ market.js
@@ -1,15 +1,36 @@
 
 // Перевірка підключеного файлу скриптів
-console.log('Перевірка підключеного файлу скриптів market.js')
+console.log('Перевірка підключеного файлу скриптів market.js');
+
+// Визначення масиву товарів
+let itemsArray = [
+    'Газонокосарка 43',
+    'Електричний тример 110',
+    'Електрична газонокосарка 32',
+    'Акумуляторний оприскувач 12N',
+]
 
 // Отримання елементу з ідентифікатором items
 let itemsDiv = document.getElementById("items");
 
+
 //Перевірка існування знайденого блоку 
 if(itemsDiv) {
+    // Виведення елементів масиву
+    itemsArray.forEach((item, index)=>{
+        // itemsDiv.innerText += item
+        // Виводимо на веб сторінку елемент масиву в блок з класом item 
+        itemsDiv.innerHTML += 
+        `
+        <div class = "item">
+            <h2>Товар №${index} з 4</h2>
+            <p>${item}</p>
+        </div>
+        `
+    })
 
     //Вивід знайденого елементу 
-    console.log(itemsDiv)
+    // console.log(itemsDiv)
     // Вивід значення поля знайденого елементу
     // console.log('Поле classList: ', itemsDiv.classList)
     // console.log('Поле id: ', itemsDiv.id)
@@ -27,8 +48,8 @@ if(itemsDiv) {
     // itemsDiv.innerHTML += '<div class = "item"></div>' 
     // itemsDiv.innerHTML += '<div class = "item"></div>'
 
-    for (let i =0; i<=100; i++){
-        itemsDiv.innerHTML += '<div class = "item"></div>'}  
+    // for (let i =0; i<=100; i++){
+    //     itemsDiv.innerHTML += '<div class = "item"></div>'}  
 
 } else {
 
